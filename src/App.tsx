9a9c807a750d492fe94e1670d4a506f9d09999d8
Hungvy24 <hungvydz2004@gmail.com>
2024-03-19 11:10:50 +0700

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './page/Home'
+import Shop from './page/Shop'
+import ProductDetail from './page/ProductDetail'
+import Login from './page/Login'
+import Register from './page/Register'
+import Notfound from './page/Notfound'
 
 function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/shop/:id' element={<ProductDetail />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='*' element={<Notfound />} />
+      </Routes>
       <Footer />
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-
+// import React from 'react'
+import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
-    <div>
-      <img src={'../../img/HP food.png'} alt='' width={100} />
-    </div>
+    <ul>
+      <li>
+        <NavLink to='/' className='nav-link'>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/shop' className='nav-link'>
+          Shop
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/login' className='nav-link'>
+          Login
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/register' className='nav-link'>
+          Register
+        </NavLink>
+      </li>
+    </ul>
   )
 }
 
